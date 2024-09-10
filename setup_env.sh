d0df9d28660f2dba32ab3a2b8e78b9f89b68e44a
0xGeN02 <manuelmateodgl02@gmail.com>
2024-09-10 17:07:15 +0200

    error in sh file

--- setup_env.sh
+++ setup_env.sh
@@ -22,6 +22,5 @@ pip install numpy mkdocs mkdocs-material
 pip install -r create3_docs/requirements.txt
 pip3 install irobot_edu_sdk
 cd irobot-edu-python-sdk && pip install .
-cd irobot_edu_sdk && pip install .
 
 echo "El entorno virtual '$ENV_DIR' está listo y las dependencias están instaladas."
